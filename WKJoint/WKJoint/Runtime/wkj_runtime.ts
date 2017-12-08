5e1b315f26c2d0ff32cb049b38b2f0604b36efc8
Mgen <m@mgenware.com>
2017-12-08 18:36:38 +0800

    Use a counter to generate promise IDs

--- WKJoint/WKJoint/Runtime/wkj_runtime.ts
+++ WKJoint/WKJoint/Runtime/wkj_runtime.ts
@@ -5,7 +5,6 @@
  * @ 2017 Mgen
  * 
  * This is the source code of runtime.js. It should NOT be included in "Copy Bundle Resources"!
- * To compile, run: tsc && uglifyjs wkj_runtime.js -cmo wkj_runtime.js
  * 
  * Inspired by http://igomobile.de/2017/03/06/wkwebview-return-a-value-from-native-code-to-javascript/
  */
@@ -24,6 +23,7 @@ class WKJCall {
 class WKJointRuntime {
   devMode: boolean;
   promises: { [id: string]: DelayedPromise<any>; } = {};
+  private promiseCounter: number = 0;
 
   beginPromise(ns: string|null, func: string|null, arg: any): Promise<any>|null {
     if (!ns || !func) {
@@ -32,7 +32,7 @@ class WKJointRuntime {
     }
 
     const promise = new Promise((resolve, reject) => {
-      const id = this.uuidv4();
+      const id = this.generateID();
       const delayedPromise = new DelayedPromise(resolve, reject);
       this.promises[id] = delayedPromise;
       this.log(`BeginPromise: ${ns}.${func} [${id}]`);
@@ -69,13 +69,10 @@ class WKJointRuntime {
     // remove reference
     delete this.promises[id];
   }
-
-  // https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-  private uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
+  
+  private generateID(): string {
+    this.promiseCounter++;
+    return `p-${this.promiseCounter}`;
   }
 
   private log(msg: string) {
