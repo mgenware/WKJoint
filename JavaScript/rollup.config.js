e2e0e7428700d0dbf8cf8381d6b24d7fa7c5be5f
Mgen <m@mgenware.com>
2017-12-16 03:34:01 +0800

    Uglify bundle.js

--- JavaScript/rollup.config.js
+++ JavaScript/rollup.config.js
@@ -1,12 +1,14 @@
 import typescript from 'rollup-plugin-typescript2';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
   input: 'lib/main.ts',
   output: {
-    file: 'dist/bundle.js',
+    file: 'dist/js_api_bundle.js',
     format: 'iife',
   },
 	plugins: [
-		typescript({ cacheRoot: (require('unique-temp-dir'))() }),
+    typescript({ cacheRoot: (require('unique-temp-dir'))() }),
+    uglify(),
 	]
 }
