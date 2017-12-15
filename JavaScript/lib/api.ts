33bc48d54cdd1b1dd6acc666791a2bba6f4f24ef
Mgen <m@mgenware.com>
2017-12-16 04:44:27 +0800

    Add api.ts

---
+++ JavaScript/lib/api.ts
@@ -0,0 +1,39 @@
+import Runtime from './runtime';
+import Namespace from './namespace';
+
+export interface IAddArgument {
+  x: number;
+  y: number;
+}
+
+export class MathNamespace extends Namespace {
+  constructor(runtime: Runtime) {
+    super('math', runtime);
+  }
+
+  add(arg: IAddArgument): Promise<number> {
+    return this.invoke('add', arg).then((result) => {
+      return result as number;
+    });
+  }
+}
+
+export class AlertNamespace extends Namespace {
+  constructor(runtime: Runtime) {
+    super('alert', runtime);
+  }
+
+  sheetAsync(): Promise<string> {
+    return this.invoke('sheetAsync', null);
+  }
+}
+
+export default class MyAPI {
+  math: MathNamespace;
+  alert: AlertNamespace;
+
+  constructor(runtime: Runtime) {
+    this.math = new MathNamespace(runtime);
+    this.alert = new AlertNamespace(runtime);
+  }
+}
