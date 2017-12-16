d2a3e7bfd243187675bcdb7643fdb3ed9deb9892
Mgen <m@mgenware.com>
2017-12-17 01:05:02 +0800

    Use a standalone view controller for webview

--- WKJoint/ViewController.swift
+++ WKJoint/ViewController.swift
@@ -7,107 +7,17 @@
 //
 
 import UIKit
-import WebKit
-
-enum MyError : Error {
-    case RuntimeError(String)
-}
 
 class ViewController: UIViewController {
-    var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        setupWebView()
-        loadDemoPage()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    func setupWebView() {
-        // view-related
-        webView = WKWebView(frame: view.bounds)
-        view.addSubview(webView)
-        
-        // implement UI Delegate
-        webView.uiDelegate = self
-        
-        // setup custom user agent
-        WKJUserAgent.updateUserAgent(webView: webView)
-        
-        // setup JavaScript APIs
-        let jsAPIEnv = MyJavaScriptAPIEnv(webView: webView)
-        jsAPIEnv.setupEnv(namespaces: [AlertNamespace(), MathNamespace()])
-    }
+    
 }
-
-extension ViewController {
-    private func loadDemoPage() {
-        let url = Bundle.main.url(forResource: "playground", withExtension: "html", subdirectory: "DemoPage")
-        let htmlString = try! String(contentsOf: url!, encoding: .utf8)
-        webView.loadHTMLString(htmlString, baseURL: Bundle.main.bundleURL.appendingPathComponent("DemoPage", isDirectory: true))
-    }
-}
-
-// Mark: - WKUIDelegate
-extension ViewController: WKUIDelegate {
-    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo,
-                 completionHandler: @escaping () -> Void) {
-        
-        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
-            completionHandler()
-        }))
-        
-        present(alertController, animated: true, completion: nil)
-    }
-    
-    
-    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo,
-                 completionHandler: @escaping (Bool) -> Void) {
-        
-        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        
-        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
-            completionHandler(true)
-        }))
-        
-        alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { (action) in
-            completionHandler(false)
-        }))
-        
-        present(alertController, animated: true, completion: nil)
-    }
-    
-    
-    func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo,
-                 completionHandler: @escaping (String?) -> Void) {
-        
-        let alertController = UIAlertController(title: nil, message: prompt, preferredStyle: .alert)
-        
-        alertController.addTextField { (textField) in
-            textField.text = defaultText
-        }
-        
-        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
-            if let text = alertController.textFields?.first?.text {
-                completionHandler(text)
-            } else {
-                completionHandler(defaultText)
-            }
-        }))
-        
-        alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { (action) in
-            completionHandler(nil)
-        }))
-        
-        present(alertController, animated: true, completion: nil)
-    }
-}
-
-
