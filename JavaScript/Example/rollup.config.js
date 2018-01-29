f0e36e562f71be00151bb2007f448d3fd9b3163a
Mgen <m@mgenware.com>
2018-01-29 17:46:34 +0800

    Bundle WKJoint runtime files from node_module

--- JavaScript/Example/rollup.config.js
+++ JavaScript/Example/rollup.config.js
@@ -1,5 +1,7 @@
 import typescript from 'rollup-plugin-typescript2';
 import uglify from 'rollup-plugin-uglify';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   input: 'lib/main.ts',
@@ -8,7 +10,12 @@ export default {
     format: 'iife',
   },
 	plugins: [
+    resolve({
+      module: true,
+      browser: true,
+    }),
+    commonjs(),
     typescript({ cacheRoot: (require('unique-temp-dir'))() }),
     uglify(),
-	]
+	],
 }
